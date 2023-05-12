e5aeeada748b39806cbb2e3bb31fa8cdfcad1230
Rachana1104 <y.rachana1104@gmail.com>
2023-05-13 01:36:34 +0530

    camer feature added

--- packages/react-native-app/screens/Docs.tsx
+++ packages/react-native-app/screens/Docs.tsx
@@ -1,63 +1,113 @@
-import { SafeAreaView } from "react-native-safe-area-context";
-import Container from "../components/Container";
-import { H1, H2, H3, H4, H5, H6 } from "../components/Headings";
-import MonoText from "../components/MonoText";
-import { Text, View } from "../components/Themed";
-import Colors from "../constants/Colors";
+import React, { useState } from 'react';
+import { Button, Image, TextInput, View, StyleSheet,Alert } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import * as MediaLibrary from 'expo-media-library';
+import Snackbar from 'react-native-snackbar';
 
 const Docs = () => {
-    return (
-        <View
-            style={{
-                height: "100%",
-                padding: 10,
-            }}
-        >
-            <View
-                style={{
-                    flex: 1,
-                    margin: 10,
-                    alignItems: "center",
-                    justifyContent: "center",
-                    borderRadius: 10,
-                }}
-            >
-                <H4
-                    additionalStyles={{
-                        marginBottom: 10,
-                        fontFamily: "Inter-Medium",
-                    }}
-                >
-                    Celo Composer
-                </H4>
-                <Text style={{ textAlign: "center", marginBottom: 15 }}>
-                    Celo Composer is built to give you headstart on your next
-                    Web3 Mobile project on Celo
-                </Text>
-                <Text style={{ textAlign: "center", marginBottom: 10 }}>
-                    {
-                        "Celo Composer has the following components built-in ConnectWallet, AccountAddress, and AccountBalance to get you to speed!"
-                    }
-                </Text>
-                <Text style={{ textAlign: "center", fontStyle: "italic" }}>
-                    {"All respective files can be found in /components folder"}
-                </Text>
-                <Container style={{ marginTop: 15 }}>
-                    <MonoText
-                        additionalStyles={{
-                            textAlign: "center",
-                            color: Colors.brand.brown,
-                            fontSize: 13,
-                        }}
-                    >
-                        {
-                            "Delete this screen by removing Docs.tsx in /screens folder"
-                        }
-                    </MonoText>
-                </Container>
-            </View>
-        </View>
-    );
+  const [imageUri, setImageUri] = useState(null);
+  const [customName, setCustomName] = useState('');
+
+  const handleCameraPress = async () => {
+    const { status } = await ImagePicker.requestCameraPermissionsAsync();
+    if (status !== 'granted') {
+      console.log('Permission to access camera denied');
+      return;
+    }
+
+    const result = await ImagePicker.launchCameraAsync();
+    if (!result.cancelled) {
+      setImageUri(result.uri);
+      promptForCustomName();
+    }
+  };
+
+  const handleGalleryPress = async () => {
+    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (status !== 'granted') {
+      console.log('Permission to access media library denied');
+      return;
+    }
+
+    const result = await ImagePicker.launchImageLibraryAsync();
+    if (!result.cancelled) {
+      setImageUri(result.uri);
+      promptForCustomName();
+    }
+  };
+
+  const promptForCustomName = () => {
+    setCustomName(''); // Clear any existing custom name
+  };
+
+  const saveToGallery = async () => {
+    const { status } = await MediaLibrary.requestPermissionsAsync();
+
+    if (status !== 'granted') {
+      console.log('Permission to access media library denied');
+      return;
+    }
+
+    const asset = await MediaLibrary.createAssetAsync(imageUri, {filename: customName})
+
+    if (asset) {
+      try {
+        await MediaLibrary.createAlbumAsync('Expo', asset, false);
+        console.log('Image saved to gallery!');
+        Alert.alert('Success', 'Image successfully uploaded to gallery');
+      } catch (error) {
+        console.log('Error saving image to gallery:', error);
+      }
+    }
+  };
+
+  
+  return (
+    <View style={styles.container}>
+      <Button title="Take Picture" onPress={handleCameraPress} />
+      <Button title="Upload from Gallery" onPress={handleGalleryPress} />
+      {imageUri && (
+        <Image source={{ uri: imageUri }} style={styles.image} />
+      )}
+      {imageUri && (
+        <TextInput
+          style={styles.textInput}
+          placeholder="Enter file name"
+          onChangeText={text => setCustomName(text)}
+          value={customName}
+        />
+      )}
+      {imageUri && (
+        <Button
+          title="Save to Gallery"
+          onPress={saveToGallery}
+          disabled={!customName}
+        />
+      )}
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    width: 200,
+    height: 200,
+    marginBottom: 20,
+  },
+  textInput: {
+    width: '80%',
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 20,
+    paddingHorizontal: 10,
+    color:"white"
+  },
+});
 
 export default Docs;
